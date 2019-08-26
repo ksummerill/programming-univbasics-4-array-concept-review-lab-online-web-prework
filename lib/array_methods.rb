4425ce7dbf87dd625e40dcc32537ac33c70523a0
Kate Summerill <kaitlynsummerill@gmail.com>
2019-08-26 21:35:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,3 @@
-# scale = [2,3,4,5]
-# scale_2 = [6,5,4,3]
-# hill = [1,2,3,4,5,4,3,2,1]
-# valley = [5,4,3,2,1,0,1,2,3,4,5]
-# zig_zag = [500,4,1000,5,250]
-# counter = 0
-
 def find_element_index(array, value_to_find)
   var = nil
   array.length.times do|index|
@@ -17,10 +10,7 @@ var
 end  
   
 
-# array of integers
 # loop through array to find max value
-
-var = index
 
 def find_max_value(array)
    while array[index] do
